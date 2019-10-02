3c5ec5b64a742db4de2ab7e3749bd958e8524e7e
Jake Moening <jmoening@imagetrend.com>
2019-10-02 10:56:47 -0500

    Last Whitespace fix

--- src/LibYear.Lib/FileTypes/CsProjFile.cs
+++ src/LibYear.Lib/FileTypes/CsProjFile.cs
@@ -1,9 +1,9 @@
 namespace LibYear.Lib.FileTypes
 {
-  public class CsProjFile : XmlProjectFile
-  {
-    public CsProjFile(string filename) : base(filename, "PackageReference", new string[] { "Include", "Update" }, "Version")
+    public class CsProjFile : XmlProjectFile
     {
+        public CsProjFile(string filename) : base(filename, "PackageReference", new string[] { "Include", "Update" }, "Version")
+        {
+        }
     }
-  }
 }

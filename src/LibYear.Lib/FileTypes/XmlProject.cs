9ce4de3ffdb1ca705825cd845d0b2efee268b80b
Ken LeFebvre <kenneth@lefebvre.us>
2019-11-07 19:53:13 -0500

    Don't bother calculating an age for unversioned packages.

--- src/LibYear.Lib/FileTypes/XmlProject.cs
+++ src/LibYear.Lib/FileTypes/XmlProject.cs
@@ -1,25 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Xml.Linq;
-using NuGet.Versioning;
 
 namespace LibYear.Lib.FileTypes
 {
-    public abstract class XmlProject : IHavePackages
-    {
-        protected readonly XDocument _xmlContents;
-        protected readonly Stream _xmlStream;
-        public IDictionary<string, SemanticVersion> Packages { get; }
+	public abstract class XmlProject : IHavePackages
+	{
+		protected readonly XDocument _xmlContents;
+		protected readonly Stream _xmlStream;
+		public IDictionary<string, SemanticVersion> Packages { get; } = new Dictionary<string, SemanticVersion>();
 
-        protected XmlProject(Stream fileStream, string elementName, string packageAttributeName, string versionAttributeName)
-        {
-            _xmlStream = fileStream;
-            _xmlContents = XDocument.Load(fileStream);
+		protected XmlProject(Stream fileStream, string elementName, string packageAttributeName, string versionAttributeName)
+		{
+			_xmlStream = fileStream;
+			_xmlContents = XDocument.Load(fileStream);
 
-            Packages = _xmlContents.Descendants(elementName)
-                .ToDictionary(d => d.Attribute(packageAttributeName)?.Value ?? d.Element(packageAttributeName)?.Value,
-                    d => SemanticVersion.Parse(d.Attribute(versionAttributeName)?.Value ?? d.Element(versionAttributeName)?.Value));
-        }
-    }
+			foreach (var reference in _xmlContents.Descendants(elementName))
+			{
+				var name = reference.Attribute(packageAttributeName)?.Value ?? reference.Element(packageAttributeName)?.Value;
+				var version = reference.Attribute(versionAttributeName)?.Value ?? reference.Element(versionAttributeName)?.Value;
+
+				if (!string.IsNullOrEmpty(name) && SemanticVersion.TryParse(version, out var semver))
+				{
+					Packages.Add(name, semver);
+				}
+				else if (System.Diagnostics.Debugger.IsAttached)
+				{
+					if (string.IsNullOrEmpty(name))
+						Console.Error.WriteLine($"Package Reference with no name!");
+					else
+						Console.Error.WriteLine($"Reference \"{name}\" has missing or invalid version: \"{version ?? "null"}\".");
+				}
+			}
+		}
+	}
 }

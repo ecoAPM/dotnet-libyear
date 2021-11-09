9319ce53cac291b5ed8a6c15e5474db6345bddc6
Steve Desmond <steve@vtsv.ca>
2021-11-09 11:23:22 -0500

    Fix PackageVersion namespace

--- src/LibYear.Lib/PackageVersion.cs
+++ src/LibYear.Lib/PackageVersion.cs
@@ -1,82 +1,85 @@
 using System;
 using NuGet.Versioning;
 
-public class PackageVersion : NuGetVersion
+namespace LibYear.Lib
 {
-    public bool IsWildcard { get; private set; }
+	public class PackageVersion : NuGetVersion
+	{
+		public bool IsWildcard { get; private set; }
 
-    public PackageVersion(string version)
-        : this(Parse(version))
-    {
-    }
+		public PackageVersion(string version)
+			: this(Parse(version))
+		{
+		}
 
-    public PackageVersion(bool isWildcard)
-        : this(0,0,0)
-    {
-        IsWildcard = isWildcard;
-    }
+		public PackageVersion(bool isWildcard)
+			: this(0,0,0)
+		{
+			IsWildcard = isWildcard;
+		}
 
-    public PackageVersion(NuGetVersion version)
-        : base(version)
-    {
-    }
+		public PackageVersion(NuGetVersion version)
+			: base(version)
+		{
+		}
 
-    public PackageVersion(int major, int minor, int patch)
-        : base(major, minor, patch)
-    {
-    }
+		public PackageVersion(int major, int minor, int patch)
+			: base(major, minor, patch)
+		{
+		}
 
-    public PackageVersion(int major, int minor, int patch, int revision)
-        : base(major, minor, patch, revision)
-    {
-    }
+		public PackageVersion(int major, int minor, int patch, int revision)
+			: base(major, minor, patch, revision)
+		{
+		}
 
-    public new static PackageVersion Parse(string version)
-    {
-        if (version.Equals("*"))
-        {
-            return new PackageVersion(true);
-        }
+		public new static PackageVersion Parse(string version)
+		{
+			if (version.Equals("*"))
+			{
+				return new PackageVersion(true);
+			}
 
-        return new PackageVersion(NuGetVersion.Parse(version));
-    }
+			return new PackageVersion(NuGetVersion.Parse(version));
+		}
 
-    public override string ToString()
-    {
-        if (string.IsNullOrEmpty(OriginalVersion) || IsSemVer2)
-        {
-            return ToString("N", VersionFormatter.Instance);
-        }
+		public override string ToString()
+		{
+			if (string.IsNullOrEmpty(OriginalVersion) || IsSemVer2)
+			{
+				return ToString("N", VersionFormatter.Instance);
+			}
 
-        return OriginalVersion;
-    }
+			return OriginalVersion;
+		}
 
-    public new virtual string ToString(string format, IFormatProvider formatProvider)
-    {
+		public new virtual string ToString(string format, IFormatProvider formatProvider)
+		{
 
-        if (formatProvider == null
-            || !TryFormatter(format, formatProvider, out string formattedString))
-        {
-            formattedString = ToString();
-        }
+			if (formatProvider == null
+			    || !TryFormatter(format, formatProvider, out string formattedString))
+			{
+				formattedString = ToString();
+			}
 
-        return formattedString;
-    }
+			return formattedString;
+		}
 
-    protected new bool TryFormatter(string format, IFormatProvider formatProvider, out string formattedString)
-    {
-        var formatted = false;
-        formattedString = null;
+		protected new bool TryFormatter(string format, IFormatProvider formatProvider, out string formattedString)
+		{
+			var formatted = false;
+			formattedString = null;
 
-        if (formatProvider != null)
-        {
-            if (formatProvider is ICustomFormatter formatter)
-            {
-                formatted = true;
-                formattedString = formatter.Format(format, this, formatProvider);
-            }
-        }
+			if (formatProvider != null)
+			{
+				if (formatProvider is ICustomFormatter formatter)
+				{
+					formatted = true;
+					formattedString = formatter.Format(format, this, formatProvider);
+				}
+			}
 
-        return formatted;
-    }
+			return formatted;
+		}
+	}
 }

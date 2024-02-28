94b665c18f24308be81cf9f1239cda108951eb1d
Steve Desmond <Steve@ecoAPM.com>
2024-02-28 15:28:42 -0500

    Add generated regex patterns for case insensitive file name matching

---
+++ src/LibYear.Core/FileTypes/FileTypePatterns.cs
@@ -0,0 +1,23 @@
+﻿using System.IO.Abstractions;
+using System.Text.RegularExpressions;
+
+namespace LibYear.Core.FileTypes;
+
+public partial class FileTypePatterns
+{
+
+	[GeneratedRegex(".csproj$", RegexOptions.IgnoreCase)]
+	public static partial Regex CSProj();
+
+	[GeneratedRegex("^Directory.Build.props$", RegexOptions.IgnoreCase)]
+	public static partial Regex DirectoryBuildProps();
+
+	[GeneratedRegex("^Directory.Build.targets$", RegexOptions.IgnoreCase)]
+	public static partial Regex DirectoryBuildTargets();
+
+	[GeneratedRegex("^Directory.Packages.props$", RegexOptions.IgnoreCase)]
+	public static partial Regex CentralPackageManagement();
+
+	[GeneratedRegex("^packages.config$", RegexOptions.IgnoreCase)]
+	public static partial Regex PackagesConfig();
+}
---
+++ test/LibYear.Core.Tests/FileTypes/FileTypePatternsTests.cs
@@ -0,0 +1,82 @@
+﻿using LibYear.Core.FileTypes;
+using Xunit;
+
+namespace LibYear.Core.Tests.FileTypes;
+
+public class FileTypePatternsTests
+{
+	[Fact]
+	public void CanMatchCsProjFiles()
+	{
+		//arrange
+		var regex = FileTypePatterns.CSProj();
+
+		//act
+		var match = regex.IsMatch("test.csproj");
+		var caseMatch = regex.IsMatch("Test.CSProj");
+
+		//assert
+		Assert.True(match);
+		Assert.True(caseMatch);
+	}
+
+	[Fact]
+	public void CanMatchPackageConfigFiles()
+	{
+		//arrange
+		var regex = FileTypePatterns.PackagesConfig();
+
+		//act
+		var match = regex.IsMatch("packages.config");
+		var caseMatch = regex.IsMatch("Packages.Config");
+
+		//assert
+		Assert.True(match);
+		Assert.True(caseMatch);
+	}
+
+	[Fact]
+	public void CanMatchDirectoryBuildPropsFiles()
+	{
+		//arrange
+		var regex = FileTypePatterns.DirectoryBuildProps();
+
+		//act
+		var match = regex.IsMatch("Directory.Build.props");
+		var caseMatch = regex.IsMatch("Directory.build.Props");
+
+		//assert
+		Assert.True(match);
+		Assert.True(caseMatch);
+	}
+
+	[Fact]
+	public void CanMatchDirectoryBuildTargetsFiles()
+	{
+		//arrange
+		var regex = FileTypePatterns.DirectoryBuildTargets();
+
+		//act
+		var match = regex.IsMatch("Directory.Build.targets");
+		var caseMatch = regex.IsMatch("Directory.build.Targets");
+
+		//assert
+		Assert.True(match);
+		Assert.True(caseMatch);
+	}
+
+	[Fact]
+	public void CanMatchCentralPackageManagementFiles()
+	{
+		//arrange
+		var regex = FileTypePatterns.CentralPackageManagement();
+
+		//act
+		var match = regex.IsMatch("Directory.Packages.props");
+		var caseMatch = regex.IsMatch("Directory.packages.Props");
+
+		//assert
+		Assert.True(match);
+		Assert.True(caseMatch);
+	}
+}
